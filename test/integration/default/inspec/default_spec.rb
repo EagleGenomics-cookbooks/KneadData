45eadd54721f07c136c0256eb1668cf73ffeeacd
Florian-EagleGenomics <florian.reisinger@eaglegenomics.com>
2016-08-03 14:27:48 +0100

    Add additional inspec tests
    Add a test to check that the setup.py script, used to install KneadData,
    is in the expected location. Add another test to check that an additional
    command (kneaddata_database) is available, which should be setup by the
    installation.

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -12,23 +12,27 @@ describe command('java -version') do
   its('stderr') { should match(/1.8/) }
 end
 
-# Check that the Trimmomatic installation directory was created successfully
+# Check that the KneadData installation directory was created successfully
 describe file('/usr/local/kneaddata_v0.5.1') do
   it { should be_directory }
 end
 
-# Check that Java was installed
+# Check that the KneadData setup file is where it should be
+describe file('/usr/local/kneaddata_v0.5.1/setup.py') do
+  it { should be_file }
+end
+
+# Check that KneadData was installed
 describe command('which kneaddata') do
   its('exit_status') { should eq 0 }
   its('stdout') { should match %r{/kneaddata} }
 end
 
-# Check that the Trimmomatic executable is where it should be
-describe file('/usr/local/kneaddata_v0.5.1/setup.py') do
-  it { should be_file }
+describe command('which kneaddata_database') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match %r{/kneaddata_database} }
 end
 
-# Check that our tool works and has the expected version
 describe command('kneaddata --version') do
   its('exit_status') { should eq 0 }
   its('stderr') { should match(/v0.5.1/) }
