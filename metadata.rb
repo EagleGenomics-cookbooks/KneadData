eb01be8c7401c31c9cc54aaab337970680e88675
Florian-EagleGenomics <florian.reisinger@eaglegenomics.com>
2016-09-12 14:10:44 +0100

    Remove the version specification from poise-python
    The defined version clashes with the version specified by cloudcli.
    Since we don't have control over cloudcli and we probably don't need
    the latest poise-python version, we remove the restriction here.

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'Apache v2.0'
 description 'Installs/Configures KneadData'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 source_url 'https://github.com/EagleGenomics-cookbooks/KneadData'
 issues_url 'https://github.com/EagleGenomics-cookbooks/KneadData/issues'
 
 depends 'apt'
-depends 'java', '~> 1.41.0'
+depends 'java', '~> 1.41'
 depends 'tarball'
-depends 'magic_shell', '~> 1.0.0'
-depends 'poise-python', '~> 1.4.0'
+depends 'magic_shell', '~> 1.0'
+depends 'poise-python'
 depends 'Bowtie'
