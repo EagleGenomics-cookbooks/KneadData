9e377bbeeb0c637b390f8ff0d7f4c32fced6a734
GreetDB <grdbaets@gmail.com>
2017-07-10 12:17:42 +0100

    cookstyle

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'Apache v2.0'
 description 'Installs/Configures KneadData'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.3'
 
 source_url 'https://github.com/EagleGenomics-cookbooks/KneadData'
 issues_url 'https://github.com/EagleGenomics-cookbooks/KneadData/issues'
